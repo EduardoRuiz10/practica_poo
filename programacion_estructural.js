bd72a0d8e64a7029140ea64f90643ba085f9749b
EduardoRuiz10 <eruizcastro58@gmail.com>
2020-09-01 15:21:48 -0500

    programacion

---
+++ programacion_estructural.js
@@ -0,0 +1,15 @@
+
+let nombre = 'edgar';
+let apellido = 'ruiz';
+let edad = 17;
+
+console.log(`hola mi nombre es: ${nombre} ${apellido} y tengo ${edad} años`); 
+
+let num = 17;
+
+function multiplicar(valor) {
+    return valor*valor;
+}
+
+let resultado= multiplicar(5)*num;
+console.log(resultado)
